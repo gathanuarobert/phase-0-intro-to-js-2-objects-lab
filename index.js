cc5b1cffd99a86a9f84683340eba187f36adcadd
Robert Gathanua <robertgathanua@gamail.com>
2024-03-21 16:45:41 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution in this file!
+// Write your solution in this file
+let employee = {
+    name: "Derek Hale",
+    streetAddress:"Broadway 29"
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    let newEmployee = {...employee}
+    newEmployee[key] = value
+
+    return newEmployee
+}
+ 
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value
+
+    return employee
+}
+
+function  deleteFromEmployeeByKey(employee, key){
+    const worker = {...employee}
+
+    delete worker[key]
+
+    return worker
+}
+
+function  destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key]
+
+    return employee
+}
+
